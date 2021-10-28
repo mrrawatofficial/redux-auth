919c421f973bdf06e71791bb7325e990b4da98e5
mrrawatofficial <rahulrawat520@gmail.com>
2021-10-28 15:32:43 +0530

    generate random color and gradient

--- src/pages/randomBG/index.js
+++ src/pages/randomBG/index.js
@@ -5,32 +5,35 @@ import "./randombg.css";
 
 const Index = () => {
 	const [color, setColor] = useState("");
+	const [gradient, setGradient] = useState("");
 	// const [textColor, setTextColor] = useState("red");
+
+	const generateGradient = () => {
+		const randomAngle = Math.floor(Math.random() * (360 - 1 + 1) + 1);
+		let color1 = generateColor();
+		let color2 = generateColor();
+		let randomGradient = `linear-gradient(${randomAngle}deg, ${color1}, ${color2})`;
+		setGradient(randomGradient);
+		setColor("");
+	};
 
 	const generateColor = () => {
 		const letters = "0123456789ABCDEF";
 		let randomColor = "";
-		let r;
-		let g;
-		let b;
 		for (var i = 0; i < 6; i++) {
 			randomColor += letters[Math.floor(Math.random() * 16)];
 		}
-		setColor(`#${randomColor}`);
-
-		//todo create a random contrast color generator
-
-		// r = (255 - parseInt(randomColor.substring(0, 2), 16)).toString(16);
-		// g = (255 - parseInt(randomColor.substring(2, 4), 16)).toString(16);
-		// b = (255 - parseInt(randomColor.substring(4, 6), 16)).toString(16);
-		// const tc = `#${r}${g}${b}`;
-		// setTextColor(tc);
-		// console.log(textColor);
+		let result = `#${randomColor}`;
+		setColor(result);
+		setGradient("");
+		return result;
 	};
 	const copyColor = () => {
-		navigator.clipboard.writeText(color).then(
+		navigator.clipboard.writeText(gradient != "" ? gradient : color).then(
 			function () {
-				toast.success(`${color} Copied Successfully`);
+				toast.success(
+					`${gradient != "" ? gradient : color} Copied Successfully`
+				);
 			},
 			function () {
 				console.log("failed");
@@ -39,18 +42,24 @@ const Index = () => {
 	};
 
 	return (
-		<section className="random-bg" style={{ background: color }}>
+		<section
+			className="random-bg"
+			style={{ background: gradient != "" ? gradient : color }}
+		>
 			<div className="box">
 				<h2 className="text-center">This color is random generated</h2>
 				<input
 					type="text"
 					readOnly
 					className="form-control my-3"
-					value={color}
+					value={gradient != "" ? gradient : color}
 				/>
 				<div className="btn-group">
-					<button className="btn btn-primary" onClick={generateColor}>
-						Generate
+					<button className="btn btn-info" onClick={generateColor}>
+						Generate Color
+					</button>
+					<button className="btn btn-warning" onClick={generateGradient}>
+						Generate Gradient
 					</button>
 					<button className="btn btn-dark" onClick={copyColor}>
 						Copy
